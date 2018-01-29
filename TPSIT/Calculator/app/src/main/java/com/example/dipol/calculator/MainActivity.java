42ac8060e87f1b8a81921ced0df02f6a77e2d66b
Cristian <ciao@gmail.com>
2018-01-29 12:56:33 +0100

    Aggiunto calcolatrice

---
+++ TPSIT/Calculator/app/src/main/java/com/example/dipol/calculator/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.dipol.calculator;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn0=findViewById(R.id.zero);
+        Button btn1=findViewById(R.id.one);
+        Button btn2=findViewById(R.id.two);
+        Button btn3=findViewById(R.id.three);
+        Button btn4=findViewById(R.id.four);
+        Button btn5=findViewById(R.id.five);
+        Button btn6=findViewById(R.id.six);
+        Button btn7=findViewById(R.id.seven);
+        Button btn8=findViewById(R.id.eight);
+        Button btn9=findViewById(R.id.nine);
+        Button btnP=findViewById(R.id.plus);
+        Button btnMi=findViewById(R.id.minos);
+        Button btnMu=findViewById(R.id.multiply);
+        Button btnD=findViewById(R.id.divide);
+        Button btnEq=findViewById(R.id.equals);
+        Button btnVir=findViewById(R.id.virg);
+        Button btnDel=findViewById(R.id.del);
+
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnMi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnMu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnEq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnVir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    /*public void send7(View view){
+        ;
+    }*/
+}
