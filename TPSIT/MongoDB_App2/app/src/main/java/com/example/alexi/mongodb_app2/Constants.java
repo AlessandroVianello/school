674d893adeebef929a66db1e7e5b47ce4f855c8b
AlessandroVianello <alex.ilre33@gmail.com>
2019-11-06 16:49:15 +0100

    Update FlatDice

--- TPSIT/MongoDB_App2/app/src/main/java/com/example/alexi/mongodb_app2/Constants.java
+++ TPSIT/MongoDB_App2/app/src/main/java/com/example/alexi/mongodb_app2/Constants.java
@@ -2,7 +2,7 @@ package com.example.alexi.mongodb_app2;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://192.168.137.131:8080/api/v1/";
+    public static final String BASE_URL = "http://192.168.10.100:8080/api/v1/";
     public static final String TOKEN = "token";
     public static final String EMAIL = "email";
     public static final String NAME = "name";
